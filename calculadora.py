04312a333cabd0ec33bee63358af243e200e31d3
Ana julia Nogueira de Lima Barboza <163192987+Najuliabarboza@users.noreply.github.com>
2024-04-16 17:10:00 -0300

    primeira alteração

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+def soma(a,b)
+return a + b
+if__name__ ==*__main__*
+num1 = float(input('Digite o primeironúmero:"))
+num2 = float(input('Digite o primeironúmero:")) 
+resultado = soma(num1,num2)
+print(f Resultado:{resultado}")
